9e68cd1dede54e0ade4a46dc0bda8653f060b87f
Tony Sorensen <tony48853@gmail.com>
2021-05-11 19:54:19 -0700

    json added to server, logger middleware working

--- api/server.js
+++ api/server.js
@@ -3,8 +3,17 @@ const express = require('express');
 const server = express();
 
 // remember express by default cannot parse JSON in request bodies
+server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
+//const usersRouter = require('./users/users-router')
+
+function methodLogger(req, res, next) {
+  console.log('Request method: ',req.method, 'Request URL: ', req.url, 'Timestamp: ',new Date().toISOString());
+
+  next();
+}
+server.use(methodLogger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
