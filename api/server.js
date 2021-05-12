737b9c55c6d2610cfa99fffd523eb25972025a81
Tony Sorensen <tony48853@gmail.com>
2021-05-11 23:25:43 -0700

    logger moved to middleware, lots of struggles

--- api/server.js
+++ api/server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-
+const logger = require ('../api/middleware/middleware')
 const server = express();
+const usersRouter = require('./users/users-router')
 
 // remember express by default cannot parse JSON in request bodies
 server.use(express.json());
 
 // global middlewares and the user's router need to be connected here
-//const usersRouter = require('./users/users-router')
 
-function methodLogger(req, res, next) {
-  console.log('Request method: ',req.method, 'Request URL: ', req.url, 'Timestamp: ',new Date().toISOString());
+// function methodLogger(req, res, next) {
+//   console.log('Request method: ',req.method, 'Request URL: ', req.url, 'Timestamp: ',new Date().toISOString());
 
-  next();
-}
-server.use(methodLogger);
-
+//   next();
+// }
+server.use(logger);
+server.use('/api/users', usersRouter)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
